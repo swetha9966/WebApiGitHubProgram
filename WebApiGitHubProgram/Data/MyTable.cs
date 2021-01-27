b6d180d76f5f0fa5058df8872dbfc5993d4b12a7
Sue <sueicecream8@gmail.com>
2021-01-26 22:17:55 -0800

    MyTable Model is created

---
+++ WebApiGitHubProgram/Data/MyTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApiGitHubProgram.Data
+{
+    public class MyTable
+    {
+       [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]// Database will generate number by db
+       // [DatabaseGenerated(DatabaseGeneratedOption.None)] //Database will not generate
+        public int Id { get; set; }
+       // [StringLength(50)] // [MinLength(3)]
+        [Required, StringLength(50), MinLength(3)]
+        public string EmpName { get; set; }
+        public double Salary { get; set; } 
+        public DateTime? DOB { get; set; }  //suffix ? means it is nullable property
+    }
+}
