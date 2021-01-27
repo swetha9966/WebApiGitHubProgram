e791c9baabbb861143edd68dca6dcb7c861ce51d
Sue <sueicecream8@gmail.com>
2021-01-26 23:03:18 -0800

    Migration added

---
+++ WebApiGitHubProgram/Migrations/20210127065626_initialmigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiGitHubProgram.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyTables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(maxLength: 50, nullable: false),
+                    Salary = table.Column<double>(nullable: false),
+                    DOB = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyTables", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyTables");
+        }
+    }
+}
